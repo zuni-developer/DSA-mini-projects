6bfc94b21608742355efc13c795cd14bffdde60a
zuni-developer <zunairasabir7@gmail.com>
2025-07-25 00:21:37 +0500

    Uploading file

---
+++ GraphMaster/Node.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, element):
+        self.element = element
+
+    def get_element(self):
+        return self.element
+
+    def __str__(self):
+        return self.element
